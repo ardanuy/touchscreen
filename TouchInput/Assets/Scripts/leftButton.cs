b198b3e3bfdaa780ee2eb5462887e384ee7b6c83
ardanuy <ardanuysagan@gmail.com>
2014-09-10 18:36:52 -0300

    adding speed based on the height of the screen

--- TouchInput/Assets/Scripts/leftButton.cs
+++ TouchInput/Assets/Scripts/leftButton.cs
@@ -1,11 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class leftButton : MonoBehaviour {
 
-	void OnTouchDown(Vector3 hitPoint){}
-	void OnTouchUp(Vector3 hitPoint){}
-	void OnTouchStay(Vector3 hitPoint){}
-	void OnTouchMove(Vector3 hitPoint){}
-	void OnTouchExit(Vector3 hitPoint){}
+
+	private Vector3 onTouchDownHitPoint;
+	private Vector3 onTouchUpHitPoint;
+	private Vector3 onTouchStayHitPoint;
+	private List<Vector3> onTouchMoveHitPoints;
+
+	public GUIText text;
+
+	private float speed;
+
+	void OnTouchDown(Vector3 hitPoint){
+		onTouchDownHitPoint = hitPoint;	
+
+		speed = (hitPoint.y / Screen.height);
+
+		text.text = ToString (speed);
+		//Debug.Log ("Speed: " + speed);
+
+	} // end of OnTouchDown
+
+
+
+	void OnTouchUp(Vector3 hitPoint){
+		onTouchUpHitPoint = hitPoint;
+	} // end of OnTouchUp
+
+
+
+	void OnTouchStay(Vector3 hitPoint){
+		onTouchStayHitPoint = hitPoint;
+	} // end of OnToucStay
+
+
+
+	void OnTouchMove(Vector3 hitPoint){
+		onTouchMoveHitPoints.Add (hitPoint);
+
+	} // end of OnTouchMove
+
+
+
+	void OnTouchExit(Vector3 hitPoint){
+		Debug.Log ("Touch canceled!!!");
+	} // end of OnTouchExit
+
+
+
+
+	// ------------------------ GET FUNCTIONS ----------------------------
+	public float getSpeed(){ return speed; }
+
 }
