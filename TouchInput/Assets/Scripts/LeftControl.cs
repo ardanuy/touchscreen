f6e1e875d1724ddbbe6def3ae34246a305a25d51
ardanuy <ardanuysagan@gmail.com>
2014-09-13 21:47:36 -0300

    refactoring a lot

---
+++ TouchInput/Assets/Scripts/LeftControl.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LeftControl : TouchInput {
+	
+	public GUIText textScreenSpecs;
+	
+	public GUIText textCurrentPoint;
+	public GUIText textPointDown;
+	public GUIText textPointUp;
+	
+	public GUIText textSpeed;
+	
+	private float speed;
+
+	private Vector2 pointDown, pointUp;
+
+	/**
+	 * 
+	 */
+	void Awake(){
+		textCurrentPoint.text = "Current (x,y)";
+		textPointDown.text = "Down (x,y)";
+		textPointUp.text = "Up (x,y)";
+		
+		textSpeed.text = "Speed:";
+		textScreenSpecs.text = "Screen Height: " + Screen.height + "   Screen Width: " + Screen.width;
+	}
+
+	/**
+	 * 
+	 */
+	void OnTouchDown(Vector2 hitPoint){
+		pointDown = hitPoint;
+		speed = (hitPoint.y / Screen.height);
+
+		// debuging
+		ShowInformation (hitPoint);
+	} // end of OnTouchDown
+	
+	
+	/**
+	 * 
+	 */
+	void OnTouchUp(Vector2 hitPoint){
+		pointUp = hitPoint;
+		speed = 0f;
+
+		// debuging
+		ShowInformation (hitPoint);
+	} // end of OnTouchUp
+	
+	
+	
+	void OnTouchStay(Vector2 hitPoint){
+		speed = (hitPoint.y / Screen.height);
+
+		// debuging
+		ShowInformation (hitPoint);
+	} // end of OnToucStay
+	
+	
+	/**
+	 * 
+	 */
+	void OnTouchMove(Vector2 hitPoint){
+		speed = (hitPoint.y / Screen.height);
+
+		// debuging
+		ShowInformation (hitPoint);
+		
+	} // end of OnTouchMove
+	
+	
+	/**
+	 * 
+	 */
+	void OnTouchExit(Vector2 hitPoint){
+	} // end of OnTouchExit
+	
+
+
+
+	/**
+	 * 
+	 */
+	// ------------------------ Debuging ---------------------------------
+	void ShowInformation(Vector2 hitPoint){
+		textSpeed.text = "Speed: " + speed;
+		textPointDown.text = "Down (" + pointDown.x + "," + pointDown.y + ")";
+		textPointUp.text = "Up (" + pointUp.x + "," + pointUp.y + ")";
+		textCurrentPoint.text = "Current (" + hitPoint.x + "," + hitPoint.y + ")";
+		
+	}
+	
+	
+	// ------------------------ GET FUNCTIONS ----------------------------
+	/**
+	 * 
+	 */
+	public float getSpeed(){ return speed; }
+	
+}
+
---
+++ TouchInput/Assets/Scripts/RightControl.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightControl : TouchInput {
+	
+	// show information about the touch
+	public GUIText textCurrentPoint;
+	public GUIText textPointDown;
+	public GUIText textPointUp;
+	public GUIText textXDistance;
+	public GUIText textYDistance;
+	public GUIText textDistance;
+	
+	public GUIText textAnimationName;
+	
+	
+	// recognize gestures
+	private Vector2 pointDown, pointUp;
+	private bool defaultJumping = false;
+	private bool touchMoving = false;
+
+	private float xDistance = 0f;
+	private float yDistance = 0f;
+	private float distance = 0f;
+
+
+	// tweaks 
+	public float xPixelThreshold = 20;
+	public float yPixelThreshold = 20;
+	public float distanceThreshold = 0f;
+
+	/**
+	 * 
+	 */
+	void Awake(){
+		textCurrentPoint.text = "Current (x,y)";
+		textPointDown.text = "Down (x,y)";
+		textPointUp.text = "Up (x,y)";
+		textAnimationName.text = "Animation Name";
+		
+		pointDown = new Vector2 (0, 0);
+		pointUp = new Vector2 (0, 0);
+	}
+
+	/**
+	 * 
+	 */
+	void OnTouchUp(Vector2 hitPoint){
+		pointUp = hitPoint;
+		// debuging
+		ShowInformation (hitPoint);
+		// update distances
+		xDistance = Mathf.Abs (pointDown.x - pointUp.x);
+		yDistance = Mathf.Abs (pointDown.y - pointUp.y);
+		distance = Vector2.Distance(pointDown, pointUp);
+
+	} // end of OnTouchUp
+	
+	/**
+	 * 
+	 */
+	void OnTouchDown(Vector2 hitPoint){
+		pointDown = hitPoint;
+		// debuging
+		ShowInformation (hitPoint);
+		
+		
+		
+	} // end of OnTouchDown
+	
+	
+	void OnTouchStay(Vector2 hitPoint){
+		touchMoving = false;
+		// debuging
+		ShowInformation (hitPoint);
+
+
+	} // end of OnToucStay
+	
+	
+	/**
+	 * 
+	 */
+	void OnTouchMove(Vector2 hitPoint){
+		// player's finger is moving
+		touchMoving = true;
+
+		// update distances
+		xDistance = Mathf.Abs (pointDown.x - hitPoint.x);
+		yDistance = Mathf.Abs (pointDown.y - hitPoint.y);
+		distance = Vector2.Distance(pointDown, hitPoint);
+
+		// debuging
+		ShowInformation (hitPoint);
+
+	} // end of OnTouchMove
+	
+	
+	/**
+	 * 
+	 */
+	void OnTouchExit(Vector2 hitPoint){
+		
+		touchMoving = false;
+		
+		pointDown = new Vector2 (0, 0);
+		pointUp = new Vector2 (0, 0);
+	} // end of OnTouchExit
+	
+	
+	
+	/**
+	 * 
+	 */
+	// ------------------------ Debuging ---------------------------------
+	void ShowInformation(Vector2 hitPoint){
+		textPointDown.text = "Down (" + pointDown.x + "," + pointDown.y + ")";
+		textPointUp.text = "Up (" + pointUp.x + "," + pointUp.y + ")";
+		textCurrentPoint.text = "Current (" + hitPoint.x + "," + hitPoint.y + ")";	
+		textXDistance.text = "X distance = " + xDistance + " pixels";
+		textYDistance.text = "Y distance = " + yDistance + " pixels";
+		textDistance.text = "Distance = " + distance;
+
+	}
+
+	// ------------------------ Gesture Recognizer ----------------------------
+	/**
+	 * 
+	 */
+	private void GestureRecognition(){
+
+
+
+	} // end of GestureRecognition
+
+	/**
+	 * 
+	 */
+	private void GestureManagement(){
+		
+	} // end of GestureManagement
+}
