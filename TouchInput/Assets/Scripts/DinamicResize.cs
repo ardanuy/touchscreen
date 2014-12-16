3078398d28bc55a2c764fadba87f72076bb8cc2c
ardanuy <ardanuysagan@gmail.com>
2014-12-15 23:57:06 -0200

    add dinamic screen resize
    making touch input textures resize for any device

---
+++ TouchInput/Assets/Scripts/DinamicResize.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DinamicResize : MonoBehaviour {
+
+	private GUITexture leftGuiTexture;
+	private GUITexture rightGuiTexture;
+	
+	// Use this for initialization
+	void Start () {
+
+		leftGuiTexture = GameObject.FindGameObjectWithTag ("GuiTextureTouchLeft").GetComponent<GUITexture> ();
+		rightGuiTexture = GameObject.FindGameObjectWithTag ("GuiTextureTouchRight").GetComponent<GUITexture> ();
+
+		// Rect (X, Y, W, H);
+		float H = Screen.height;
+		float W = Screen.width / 2;
+		float Y = (Screen.height / 2) * -1;
+
+		float xLeft = (Screen.width / 2) * -1;
+		float xRight = 0;
+
+		// Rect (X, Y, W, H);
+		leftGuiTexture.pixelInset = new Rect (xLeft, Y, W, H);
+		rightGuiTexture.pixelInset = new Rect (xRight, Y, W, H);
+	}
+
+}
